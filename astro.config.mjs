67a6ea67d41f6c34c10f4ba37ed08de5ae39bcec
siriuns-x <siriuns@icloud.com>
2026-01-12 20:31:10 +0800

    feat(latex): add latex support and test post

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,16 @@
 import { defineConfig } from 'astro/config';
 
+// === @USER_ADD: LaTeX Support Dependencies START ===
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
+// === @USER_ADD END ===
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    // === @USER_ADD: LaTeX Markdown Engine START ===
+    markdown: {
+        remarkPlugins: [remarkMath],
+        rehypePlugins: [rehypeKatex],
+    }
+    // === @USER_ADD END ===
+});
---
+++ src/pages/posts/latex-test.md
@@ -0,0 +1,20 @@
+---
+layout: ../../layouts/MarkdownPostLayout.astro
+title: 'A test page for latex'
+pubDate: 2026-1-12
+description: 'just nothing.'
+author: 'Siriuns'
+tags: ["latex", "test"]
+---
+
+## Gamma function
+
+$$
+\Gamma(z) = \int_{0}^{+\infty} t^{z-1} e^{-t} \, \mathrm{d}t \\
+$$
+
+## Stokes' theorem
+
+$$
+\int_{\Omega} \mathrm{d}\omega = \int_{\partial\Omega} \omega
+$$
