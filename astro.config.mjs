1ec10907723d8d5c754e33c8a649e4bb797c51cb
Siriuns-X <Siriuns@icloud.com>
2026-09-18 00:09:10 +0800

    chore: remove redundant explanatory comments

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,32 +1,17 @@
 import { defineConfig } from 'astro/config';
-
-// #region @USER_ADD: LaTeX Support Dependencies
 import { unified } from "@astrojs/markdown-remark";
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
-// #endregion
 import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
     markdown: {
-        // #region @USER_ADD: LaTeX Markdown Engine
         processor: unified({
             remarkPlugins: [remarkMath],
             rehypePlugins: [
-                [
-                    rehypeKatex,
-                    {
-                        macros: {
-                            "\\dd": "\\mathop{}\\!\\mathrm{d}",
-                        },
-                    },
-                ],
+                [rehypeKatex, { macros: { "\\dd": "\\mathop{}\\!\\mathrm{d}", }, },],
             ],
         }),
-        // #endregion
-
-        // #region @USER_ADD: Code Highlight Engine
         shikiConfig: {
             defaultColor: false,
             themes: {
@@ -34,11 +19,8 @@ export default defineConfig({
                 dark: "github-dark",
             },
         },
-        // #endregion
-    },
 
-    // #region @USER_ADD: RSS And Sitemap
+    },
     site: 'https://siriuns.netlify.app',
     integrations: [sitemap()],
-    // #endregion
 });
